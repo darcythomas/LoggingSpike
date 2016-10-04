45b2ff961d9599dfe05b1a1445c10e0c7e86c554
Darcy <github@darcythomas.com>
2016-10-04 21:23:57 +1300

    wip

--- OldSchool/OldSchool/Startup.cs
+++ OldSchool/OldSchool/Startup.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Owin;
 using Owin;
+using Serilog;
 
 [assembly: OwinStartupAttribute(typeof(OldSchool.Startup))]
 namespace OldSchool
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration(IAppBuilder app, ILoggerFactory loggerFactory)
         {
             ConfigureAuth(app);
+
+            Log.Logger = new LoggerConfiguration()
+     .Enrich.FromLogContext()
+     .WriteTo.LiterateConsole()
+     .CreateLogger();
+
+
+
+            loggerFactory.AddSerilog();
         }
     }
 }
